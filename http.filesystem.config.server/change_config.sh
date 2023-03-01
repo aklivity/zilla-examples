64539d43de87653ac871c149b0d259653a887f5b
bmaidics <b.maidics@gmail.com>
2023-03-01 15:59:04 +0100

    Config server example

--- http.filesystem.config.server/change_config.sh
+++ http.filesystem.config.server/change_config.sh
@@ -2,8 +2,10 @@
 set -ex
 
 # change configfile
-ZILLA_POD=$(kubectl get pods --namespace zilla-config-server --selector app.kubernetes.io/instance=zilla-config -o json | jq -r '.items[0].metadata.name')
-kubectl cp --namespace zilla-config-server zilla_changed.yaml "$ZILLA_POD:/var/www/zilla.yaml"
+#ZILLA_POD=$(kubectl get pods --namespace zilla-config-server --selector app.kubernetes.io/instance=zilla-config -o json | jq -r '.items[0].metadata.name')
+#kubectl cp --namespace zilla-config-server zilla_changed.yaml "$ZILLA_POD:/var/www/zilla.yaml"
+kubectl --namespace zilla-config-server create configmap configmap-served --from-file zilla.yaml -o yaml --dry-run | kubectl apply -f -
+
 
 # Start port forwarding
 kubectl port-forward --namespace zilla-config-server service/zilla-http 8088 > /tmp/kubectl-zilla-config-change.log 2>&1 &

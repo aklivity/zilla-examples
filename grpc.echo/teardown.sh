03ac74439136ae1a3885b6aded13297181b18a7f
Akram  Yakubov <akram.yakubov@gmail.com>
2023-03-16 21:56:43 -0700

    Revert "Grpc echo and proxy server."
    This reverts commit f597f020a8d1d78ca05b55f003c0c9d9216b28c0.

--- grpc.echo/teardown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-
-# Stop port forwarding
-pgrep kubectl && killall kubectl
-
-# Uninstall Zilla engine
-helm uninstall zilla-grpc-echo --namespace zilla-grpc-echo
-kubectl delete namespace zilla-grpc-echo

dc5c0e23c7584b5df576ad268965c309124ba3ad
Attila Kreiner <kreiner.attila@gmail.com>
2023-05-12 11:32:10 +0200

    http.echo.jwt

--- http.echo.jwt/README.md
+++ http.echo.jwt/README.md
@@ -28,15 +28,17 @@ The `setup.sh` script:
 
 ```bash
 $ ./setup.sh
-+ helm install zilla-http-echo-jwt chart --namespace zilla-http-echo-jwt --create-namespace --wait
++ ZILLA_CHART=../zilla-0.1.0-develop-SNAPSHOT.tgz
++ helm install zilla-http-echo-jwt chart --namespace zilla-http-echo-jwt --create-namespace --wait [...]
 NAME: zilla-http-echo-jwt
 LAST DEPLOYED: [...]
 NAMESPACE: zilla-http-echo-jwt
 STATUS: deployed
 REVISION: 1
 TEST SUITE: None
+[...]
 + nc -z localhost 8080
-+ kubectl port-forward --namespace zilla-http-echo-jwt service/zilla 8080 9090
++ kubectl port-forward --namespace zilla-http-echo-jwt service/zilla-http-echo-jwt 8080 9090
 + sleep 1
 + nc -z localhost 8080
 Connection to localhost port 8080 [tcp/http-alt] succeeded!
--- http.echo.jwt/setup.sh
+++ http.echo.jwt/setup.sh
@@ -2,8 +2,12 @@
 set -ex
 
 # Install Zilla to the Kubernetes cluster with helm and wait for the pod to start up
-helm install zilla-http-echo-jwt chart --namespace zilla-http-echo-jwt --create-namespace --wait
+ZILLA_CHART=../zilla-0.1.0-develop-SNAPSHOT.tgz
+helm install zilla-http-echo-jwt $ZILLA_CHART --namespace zilla-http-echo-jwt --create-namespace --wait \
+    --values values.yaml \
+    --set-file zilla\\.yaml=zilla.yaml \
+    --set-file secrets.tls.data.localhost\\.p12=tls/localhost.p12
 
 # Start port forwarding
-kubectl port-forward --namespace zilla-http-echo-jwt service/zilla 8080 9090 > /tmp/kubectl-zilla.log 2>&1 &
+kubectl port-forward --namespace zilla-http-echo-jwt service/zilla-http-echo-jwt 8080 9090 > /tmp/kubectl-zilla.log 2>&1 &
 until nc -z localhost 8080; do sleep 1; done

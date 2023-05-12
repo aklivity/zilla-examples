e44ad316170bd2ffa8a48b11efb6555c3e43dd7e
Attila Kreiner <kreiner.attila@gmail.com>
2023-05-12 18:57:12 +0200

    kubernetes.prometheus.autoscale

--- kubernetes.prometheus.autoscale/setup.sh
+++ kubernetes.prometheus.autoscale/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -ex
 
-# Install Zilla to the Kubernetes cluster with helm and wait for the pod to start up
-helm install zilla-kubernetes-prometheus-autoscale chart --namespace zilla-kubernetes-prometheus-autoscale --create-namespace --wait
+# Install Zilla to the Kubernetes cluster with helm and wait for the pods to start up
+ZILLA_CHART=../zilla-0.1.0-develop-SNAPSHOT.tgz
+helm install zilla-kubernetes-prometheus-autoscale $ZILLA_CHART --namespace zilla-kubernetes-prometheus-autoscale --create-namespace --wait \
+    --values values.yaml \
+    --set-file zilla\\.yaml=zilla.yaml
+
+# Install Prometheus and Prometheus Adapter to the Kubernetes cluster with helm and wait for the pod to start up
+helm install zilla-kubernetes-prometheus-autoscale-prometheus chart --namespace zilla-kubernetes-prometheus-autoscale --create-namespace --wait
 
 # Start port forwarding
-kubectl port-forward --namespace zilla-kubernetes-prometheus-autoscale service/zilla 8080 > /tmp/kubectl-zilla.log 2>&1 &
+kubectl port-forward --namespace zilla-kubernetes-prometheus-autoscale service/zilla-kubernetes-prometheus-autoscale 8080 > /tmp/kubectl-zilla.log 2>&1 &
 kubectl port-forward --namespace zilla-kubernetes-prometheus-autoscale service/prometheus 9090 > /tmp/kubectl-prometheus.log 2>&1 &
 until nc -z localhost 8080; do sleep 1; done
 until nc -z localhost 9090; do sleep 1; done

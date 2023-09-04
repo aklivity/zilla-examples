b4138a5ed8eb87c2f93e54ae8bfd4e491dd3509b
AJ <vordimous@outlook.com>
2023-09-04 09:42:28 -0400

    add simulator

---
+++ quickstart/mqtt/mqtt-simulator/simulator.py
@@ -0,0 +1,44 @@
+import json
+from topic import TopicAuto
+
+class Simulator:
+    def __init__(self, settings_file):
+        self.broker_url = None
+        self.broker_port = None
+        self.topics = []
+        self.load_settings(settings_file)
+
+    def load_settings(self, settings_file):
+        with open(settings_file) as json_file:
+            config = json.load(json_file)
+            self.broker_url = config['BROKER_URL']
+            self.broker_port = config['BROKER_PORT']
+            # read each configured topic
+            for topic in config['TOPICS']:
+                topic_data = topic['DATA']
+                topic_time_interval = topic['TIME_INTERVAL']
+                if topic['TYPE'] == 'single':
+                    # create single topic with format: /{PREFIX}
+                    topic_url = topic['PREFIX']
+                    self.topics.append(TopicAuto(self.broker_url, self.broker_port, topic_url, topic_data, topic_time_interval))
+                elif topic['TYPE'] == 'multiple':
+                    # create multiple topics with format: /{PREFIX}/{id}
+                    for id in range(topic['RANGE_START'], topic['RANGE_END']+1):
+                        topic_url = topic['PREFIX'] + '/' + str(id)
+                        self.topics.append(TopicAuto(self.broker_url, self.broker_port, topic_url, topic_data, topic_time_interval))
+                elif topic['TYPE'] == 'list':
+                    # create multiple topics with format: /{PREFIX}/{item}
+                    for item in topic['LIST']:
+                        topic_url = topic['PREFIX'] + '/' + str(item)
+                        self.topics.append(TopicAuto(self.broker_url, self.broker_port, topic_url, topic_data, topic_time_interval))
+                    
+
+    def run(self):
+        for topic in self.topics:
+            print(f'Starting: {topic.topic_url} ...')
+            topic.start() 
+
+    def stop(self):
+        for topic in self.topics:
+            print(f'Stopping: {topic.topic_url} ...')
+            topic.stop() 
---
+++ quickstart/mqtt/mqtt-simulator/topic.py
@@ -0,0 +1,126 @@
+import time
+import json
+import random
+import threading
+from abc import ABC, abstractmethod
+import paho.mqtt.client as mqtt
+import paho.mqtt.properties as props
+from paho.mqtt.packettypes import PacketTypes
+from expression_evaluator import ExpressionEvaluator
+
+class Topic(ABC):
+    def __init__(self, broker_url, broker_port, topic_url, topic_data):
+        self.broker_url = broker_url
+        self.broker_port = broker_port
+        self.topic_url = topic_url
+        self.topic_data = topic_data
+        self.client = None
+
+    def connect(self):
+        self.client = mqtt.Client(self.topic_url, transport='tcp', protocol=mqtt.MQTTv5)
+        self.client.on_message = self.on_message
+        self.client.on_publish = self.on_publish
+        self.client.on_connect = self.on_connect
+        self.client.on_connect_fail = self.on_connect_fail
+
+        self.client.connect(self.broker_url, self.broker_port)
+        self.client.loop_start()
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    def disconnect(self):
+        self.client.loop_end()
+        self.client.disconnect()
+
+    def on_message(client, userdata, message):
+        msg=str(message.payload.decode("utf-8"))
+        print("correlation=",message.properties)
+        print('RECV Topic = ',message.topic)
+        print('RECV MSG =', msg)
+        print("properties received= ",message.properties)
+        print("user properties received= ",message.properties)
+
+    def on_publish(self, client, userdata, result):
+        print(f'[{time.strftime("%H:%M:%S")}] Data published on: {self.topic_url}')
+
+    def on_connect(self, client, _, flags, return_code):
+        print("MQTT connected")
+        # print(f'Data published on: {flags}')
+        # print("MQTT connection flags: %s", flags)
+        # print("MQTT connection returned result: %s", mqtt.connack_string(return_code))
+
+    def on_connect_fail(self, client, _, flags, return_code):
+        print("MQTT connection failed")
+        # print(f'Data published on: {flags}')
+        # print("MQTT connection flags: %s", flags)
+        # print("MQTT connection returned result: %s", mqtt.connack_string(return_code))
+
+
+class TopicAuto(Topic, threading.Thread):
+    def __init__(self, broker_url, broker_port, topic_url, topic_data, time_interval):
+        Topic.__init__(self, broker_url, broker_port, topic_url, topic_data)
+        threading.Thread.__init__(self, args = (), kwargs = None)
+        self.time_interval = time_interval
+        self.old_payload = None
+        self.expression_evaluators = {}
+
+    def run(self):
+        self.connect()
+        while True:
+            payload = self.generate_payload()
+            self.old_payload = payload
+            publish_properties = props.Properties(PacketTypes.PUBLISH)
+            publish_properties.UserProperty = ("content-type", "application/json")
+            publish_properties.UserProperty = ("content-encoding", "UTF-8")
+            publish_properties.UserProperty = ("Idempotency-Key", self.topic_url)
+            self.client.publish(topic=self.topic_url, payload=json.dumps(payload), qos=0, retain=True, properties=publish_properties)
+            time.sleep(self.time_interval)
+
+    def generate_initial_value(self, data):
+        if 'INITIAL_VALUE' in data:
+            return data['INITIAL_VALUE']
+        elif data['TYPE'] == 'int':
+            return random.randint(data['MIN_VALUE'], data['MAX_VALUE'])
+        elif data['TYPE'] == 'float':
+            return random.uniform(data['MIN_VALUE'], data['MAX_VALUE'])
+        elif data['TYPE'] == 'bool':
+            return random.choice([True, False])
+        elif data['TYPE'] == 'math_expression':
+            self.expression_evaluators[data['NAME']] = ExpressionEvaluator(data['MATH_EXPRESSION'], data['INTERVAL_START'], data['INTERVAL_END'], data['MIN_DELTA'], data['MAX_DELTA'])
+            return self.expression_evaluators[data['NAME']].get_current_expression_value()
+            
+    def generate_next_value(self, data, old_value):
+        if 'RESET_PROBABILITY' in data and random.random() < data['RESET_PROBABILITY']:
+            return self.generate_initial_value(data)
+        if 'RESTART_ON_BOUNDARIES' in data and data['RESTART_ON_BOUNDARIES'] and (old_value == data['MIN_VALUE'] or old_value == data['MAX_VALUE']):
+            return self.generate_initial_value(data)
+        if random.random() < data['RETAIN_PROBABILITY']:
+            return old_value
+        if data['TYPE'] == 'bool':
+            return not old_value
+        elif data['TYPE'] == 'math_expression':
+            return self.expression_evaluators[data['NAME']].evaluate_expression()
+        else:
+            # generating value for int or float
+            step = random.uniform(0, data['MAX_STEP'])
+            step = round(step) if data['TYPE'] == 'int' else step
+            increase_probability = data['INCREASE_PROBABILITY'] if 'INCREASE_PROBABILITY' in data else 0.5
+            if random.random() < (1 - increase_probability):
+                step *= -1
+            return max(old_value + step, data['MIN_VALUE']) if step < 0 else min(old_value + step, data['MAX_VALUE'])
+
+    def generate_payload(self):
+        payload = {
+            "topic": self.topic_url
+        }
+        if self.old_payload == None:
+            # generate initial data
+            for data in self.topic_data:
+                payload[data['NAME']] = self.generate_initial_value(data)
+        else:
+            # generate next data
+            for data in self.topic_data:
+                payload[data['NAME']] = self.generate_next_value(data, self.old_payload[data['NAME']])
+        return payload

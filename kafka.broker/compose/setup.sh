ba07d6f2e0de71b47eb3e078b9357055670d951b
AJ <vordimous@outlook.com>
2023-10-27 15:05:39 -0400

    conditionally restart zilla

--- kafka.broker/compose/setup.sh
+++ kafka.broker/compose/setup.sh
@@ -2,5 +2,12 @@
 set -e
 
 NAMESPACE=zilla-kafka-broker
-docker-compose -p $NAMESPACE down --remove-orphans
-docker-compose -p $NAMESPACE up -d
+
+if [[ -z `docker-compose -p $NAMESPACE ps -q kafka` || -z `docker-compose -p $NAMESPACE ps -q kafka-ui` ]]; then
+  docker-compose -p $NAMESPACE pull
+  docker-compose -p $NAMESPACE up -d
+fi
+
+if [[ ! -z `docker-compose -p $NAMESPACE ps -q kafka-ui` ]]; then
+  docker-compose -p $NAMESPACE restart kafka-ui
+fi
--- mqtt.kafka.broker/compose/setup.sh
+++ mqtt.kafka.broker/compose/setup.sh
@@ -4,8 +4,6 @@ set -e
 [[ -z "$KAFKA_HOST" && -z "$KAFKA_PORT" ]] && printf "==== This example requires a running kafka instance ====\n$USAGE" && exit 0;
 
 NAMESPACE=zilla-mqtt-kafka-broker
-# Clear out any old containers
-docker-compose -p $NAMESPACE down --remove-orphans
 
 # Create the mqtt topics in Kafka
 docker run --rm bitnami/kafka:3.2 bash -c "
@@ -15,5 +13,10 @@ echo 'Creating topics for $KAFKA_HOST:$KAFKA_PORT'
 /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server $KAFKA_HOST:$KAFKA_PORT --create --if-not-exists --topic mqtt-retained --config cleanup.policy=compact
 "
 
-# Start Zilla
-docker-compose -p $NAMESPACE up -d
+# Start or restart Zilla
+if [[ -z `docker-compose -p $NAMESPACE ps -q zilla` ]]; then
+  docker-compose -p $NAMESPACE pull
+  docker-compose -p $NAMESPACE up -d
+else
+  docker-compose -p $NAMESPACE restart --no-deps zilla
+fi

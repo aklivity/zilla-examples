06f019bf4d5e02a3749cd082cbf97377067a9d02
AJ <vordimous@outlook.com>
2023-11-15 12:00:44 -0500

    setting env values from cli still needs escaped quotes

--- mqtt.kafka.broker/k8s/helm/setup.sh
+++ mqtt.kafka.broker/k8s/helm/setup.sh
@@ -10,10 +10,10 @@ fi
 # Install Zilla to the Kubernetes cluster with helm and wait for the pod to start up
 NAMESPACE=zilla-mqtt-kafka-broker
 ZILLA_CHART=oci://ghcr.io/aklivity/charts/zilla
-echo "Installing $ZILLA_CHART to $NAMESPACE"
+echo "Installing $ZILLA_CHART to $NAMESPACE with Kafka at $KAFKA_HOST:$KAFKA_PORT"
 helm upgrade --install zilla $ZILLA_CHART --namespace $NAMESPACE --create-namespace --wait \
     --values values.yaml \
-    --set extraEnv[1].value="$KAFKA_HOST",extraEnv[2].value="$KAFKA_PORT" \
+    --set extraEnv[1].value="\"$KAFKA_HOST\"",extraEnv[2].value="\"$KAFKA_PORT\"" \
     --set-file zilla\\.yaml=../../zilla.yaml \
     --set-file secrets.tls.data.localhost\\.p12=../../tls/localhost.p12
 

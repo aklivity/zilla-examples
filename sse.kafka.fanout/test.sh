99a2f8eec5f5a1accb412631153043c1209a1f0a
AJ <vordimous@outlook.com>
2024-11-08 12:15:38 -0500

    fix test

--- sse.kafka.fanout/test.sh
+++ sse.kafka.fanout/test.sh
@@ -6,8 +6,8 @@ EXIT=0
 # GIVEN
 PORT="7114"
 KAFKA_BOOTSTRAP_SERVER="kafka:29092"
-INPUT='{"id": 1,"name":"Hello World!"}'
-EXPECTED='data:{ "id": 1, "name": "Hello World!"}'
+INPUT='{"id":1,"name":"Hello World!"}'
+EXPECTED='data:{"id":1,"name":"Hello World!"}'
 echo \# Testing sse.kafka.fanout/
 echo PORT="$PORT"
 echo KAFKA_BOOTSTRAP_SERVER="$KAFKA_BOOTSTRAP_SERVER"
@@ -21,7 +21,7 @@ timeout 3s curl -N --http2 -H "Accept:text/event-stream" "http://localhost:$PORT
 
 # push response to kafka with kafkacat
 echo "$INPUT" |
-  docker compose -p zilla-http-kafka-sync exec -T kafkacat \
+  docker compose -p zilla-sse-kafka-fanout exec -T kafkacat \
     kafkacat -P \
     -b $KAFKA_BOOTSTRAP_SERVER \
     -t events \

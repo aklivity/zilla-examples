50e6be5b293fb5fe7a67b4316f66be1d97a34e7b
Ankit Kumar <ankitjames25@gmail.com>
2025-02-25 16:53:45 +0530

    fix: `grpc.proxy`

--- grpc.proxy/test.sh
+++ grpc.proxy/test.sh
@@ -16,7 +16,7 @@ echo EXPECTED="$EXPECTED"
 echo
 
 # WHEN
-OUTPUT=$(docker run --rm -v ./echo.proto:/echo.proto fullstorydev/grpcurl -insecure -proto echo.proto  -d "$INPUT" host.docker.internal:$PORT grpc.examples.echo.Echo.UnaryEcho)
+OUTPUT=$(docker run --network zilla-grpc-proxy_default --rm -v ./echo.proto:/echo.proto fullstorydev/grpcurl -insecure -proto echo.proto  -d "$INPUT" zilla:$PORT grpc.examples.echo.Echo.UnaryEcho)
 RESULT=$?
 echo RESULT="$RESULT"
 # THEN
@@ -70,7 +70,7 @@ echo EXPECTED="$EXPECTED"
 echo
 
 # WHEN
-OUTPUT=$(docker run --rm -v ./echo.proto:/echo.proto fullstorydev/grpcurl -insecure -proto echo.proto  -d "$INPUT" host.docker.internal:$PORT grpc.examples.echo.Echo.ServerStreamingEcho)
+OUTPUT=$(docker run --network zilla-grpc-proxy_default --rm -v ./echo.proto:/echo.proto fullstorydev/grpcurl -insecure -proto echo.proto  -d "$INPUT" zilla:$PORT grpc.examples.echo.Echo.ServerStreamingEcho)
 RESULT=$?
 echo RESULT="$RESULT"
 # THEN
@@ -83,9 +83,5 @@ else
   echo ❌
   EXIT=1
 fi
-
-# TODO remove once fixed
-echo '❌ Tested on main. and does not work with described instructions'
-EXIT=1
 
 exit $EXIT

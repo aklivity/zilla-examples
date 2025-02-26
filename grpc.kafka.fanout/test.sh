5b7651025ee58f73299a1eb6fc8e30078f6ddbc9
Ankit Kumar <ankitjames25@gmail.com>
2025-02-26 16:48:04 +0530

    fix `grpc.kafka.fanout`

--- grpc.kafka.fanout/test.sh
+++ grpc.kafka.fanout/test.sh
@@ -16,6 +16,9 @@ echo EXPECTED="$EXPECTED"
 echo
 
 # WHEN
+
+sleep 5
+
 (docker compose -p zilla-grpc-kafka-fanout exec kafkacat kafkacat -P -b kafka:29092 -t messages -k -e /tmp/binary.data)
 
 OUTPUT=$(timeout 3s docker run --network zilla-grpc-kafka-fanout_default --rm -v ./fanout.proto:/fanout.proto fullstorydev/grpcurl -plaintext -proto fanout.proto  -d '' zilla:$PORT example.FanoutService.FanoutServerStream)

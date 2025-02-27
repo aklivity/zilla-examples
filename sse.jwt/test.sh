f2a124d33d0295fe90ff7b15bafa16d999cc7a6b
Ankit Kumar <ankitjames25@gmail.com>
2025-02-27 19:15:04 +0530

    add `test.sh` : `sse.jwt`

---
+++ sse.jwt/test.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -x
+
+EXIT=0
+PORT="7143"
+STREAM_URL="https://localhost:$PORT/events"
+NC_PORT="7001"
+MESSAGE="Hello, world $(date)"
+
+echo \# Testing sse.jwt
+echo "PORT=$PORT"
+echo "STREAM_URL=$STREAM_URL"
+echo "NC_PORT=$NC_PORT"
+echo "MESSAGE=$MESSAGE"
+
+JWT_TOKEN=$(docker run -i --rm \
+    --name jwt-cli \
+    -v "./private.pem:/private.pem" \
+    bitnami/jwt-cli encode \
+    --alg "RS256" \
+    --kid "example" \
+    --iss "https://auth.example.com" \
+    --aud "https://api.example.com" \
+    --exp=+1d \
+    --no-iat \
+    --payload "scope=proxy:stream" \
+    --secret @/private.pem | tr -d '\r\n')
+
+OUTPUT=$(timeout 5s curl --cacert test-ca.crt --no-buffer -N "$STREAM_URL?access_token=${JWT_TOKEN}" &
+
+  SUB_PID=$!
+
+  sleep 1
+
+  echo '{ "data": "'"$MESSAGE"'" }' | nc -c localhost $NC_PORT
+
+  wait $SUB_PID
+)
+
+if echo "$OUTPUT" | grep -q "$MESSAGE"; then
+  echo ✅
+else
+  echo ❌
+  EXIT=1
+fi
+
+exit $EXIT

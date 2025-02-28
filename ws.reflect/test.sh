e708cf9686fc9b675d03b980f2c65be246626cd2
Ankit Kumar <ankitjames25@gmail.com>
2025-02-28 15:30:18 +0530

    update `ws`

--- ws.reflect/test.sh
+++ ws.reflect/test.sh
@@ -5,32 +5,22 @@ EXIT=0
 
 # GIVEN
 PORT="7114"
-INPUT1="Hello from client 1"
-INPUT2="Hello from client 2"
-
+INPUT="Hello, world"
+EXPECTED="Hello, world"
 echo \# Testing ws.reflect
 echo PORT="$PORT"
-echo INPUT1="$INPUT1"
-echo INPUT2="$INPUT2"
+echo INPUT="$INPUT"
+echo EXPECTED="$EXPECTED"
 
 # WHEN
-{
-  (echo "$INPUT1"; sleep 2) | timeout 2s docker compose -p zilla-ws-reflect exec -T websocat websocat --protocol echo ws://zilla:$PORT/ &
-  PID1=$!
-  (echo "$INPUT2"; sleep 2) | timeout 2s docker compose -p zilla-ws-reflect exec -T websocat websocat --protocol echo ws://zilla:$PORT/ &
-  PID2=$!
-  wait $PID1 $PID2
-} > output.out 2>&1
-
-RESULT1=$?
-RESULT2=$?
-OUTPUT=$(cat output.out)
-
+OUTPUT=$(echo "$INPUT" | docker compose -p zilla-ws-echo exec -T websocat websocat --protocol echo ws://zilla:7114/)
+RESULT=$?
+echo RESULT="$RESULT"
 # THEN
-COUNT1=$(echo "$OUTPUT" | grep -Fx "$INPUT1" | wc -l)
-COUNT2=$(echo "$OUTPUT" | grep -Fx "$INPUT2" | wc -l)
-
-if [ "$COUNT1" -eq 2 ] && [ "$COUNT2" -eq 2 ]; then
+echo OUTPUT="$OUTPUT"
+echo EXPECTED="$EXPECTED"
+echo
+if [ "$RESULT" -eq 0 ] && [ "$OUTPUT" = "$EXPECTED" ]; then
   echo ✅
 else
   echo ❌
